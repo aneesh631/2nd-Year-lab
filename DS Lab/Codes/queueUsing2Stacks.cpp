6b457b9a74cd2f7f7db7749641a5ebf94d2c5290
aneesh631 <aneesh.kundu14@gmail.com>
2018-09-09 22:01:28 +0530

    Added doc 4, and 5 in OOP lab and some codes in DS LAB

---
+++ DS Lab/Codes/queueUsing2Stacks.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+struct Queue{
+	stack<int> a,b;
+	void insert(int v) {
+		a.push(v);
+	}
+	int remove(){
+		while(!a.empty()){
+			b.push(a.top());
+			a.pop();
+		}
+		int ans=-1;
+		if(!b.empty()){
+			ans=b.top();
+			b.pop();
+		}
+		while(!b.empty()){
+			a.push(b.top());
+			b.pop();
+		}
+		return ans;
+	}
+};
+int main(){
+	Queue q;
+	int temp;
+	cin>>temp;
+	while(temp!=-1){
+		q.insert(temp);
+		cin>>temp;
+	}
+	while(1){
+		int top=q.remove();
+		if(top==-1)
+			break;
+		cout<<top<<endl;
+	}
+	return 0;
+}
--- OOP Lab/Codes/friend.cpp
+++ OOP Lab/Codes/friend.cpp
@@ -7,32 +7,25 @@ class friendfunc2;
 class friendfunc1 {
 	int a,b;
 public:
-	friendfunc1(int n1,int n2){
-		a=n1;
-		b=n2;
+	friendfunc1(){
+		cin>>a>>b;
 	}
 	friend float avg(friendfunc1,friendfunc2);
 };
-
 class friendfunc2 {
 	int a,b,c;
 public:
-	friendfunc2(int n1,int n2,int n3){
-		a=n1;
-		b=n2;
-		c=n3;
+	friendfunc2(){
+		cin>>a>>b>>c;
 	}
 	friend float avg(friendfunc1,friendfunc2);
 };
-
 float avg (friendfunc1 f1,friendfunc2 f2){
 	return (f1.a+f1.b+f2.a+f2.b+f2.c)/5.0;
 }
-
-
 int main(){
-	friendfunc1 a(1,2);
-	friendfunc2 b(1,2,3);
+	friendfunc1 a;
+	friendfunc2 b;
 	cout<<avg(a,b)<<endl;
 	return 0;
 }
