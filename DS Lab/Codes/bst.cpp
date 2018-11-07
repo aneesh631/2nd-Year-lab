1699d2b1c3f5953861fed8cbe627f9a149e665ca
aneesh631 <aneesh.kundu14@gmail.com>
2018-11-08 00:31:50 +0530

    added bst insertion and deletion

--- DS Lab/Codes/bst.cpp
+++ DS Lab/Codes/bst.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 class BST;
 class Node{
@@ -28,20 +29,61 @@ public:
 };
 class BST{
 	Node* root;
-	Pair find_parent(int v,Node *s){
-		if(s == NULL)
-			return Pair(false,NULL);
-		Node *prev = NULL, *cur = s;
-		while(cur != NULL){
-			if(cur->data == v)
-				return Pair(true,prev);
-			else if(cur->data < v)
-				cur = cur->right;
-			else
-				cur = cur->left;
-			prev = cur;
+	void remove_node(Node*& root,Node* p,int v){
+		if(root == NULL) return;
+		Node* c = root;
+		while(c != NULL){
+			if(c->data == v) break;
+			else if(c->data < v){
+				p = c;
+				c = c->right;
+			}
+			else{
+				p = c;
+				c = c->left;
+			}
 		}
-		return Pair(false,prev);
+		if(c){
+			if(c->left == NULL && c->right == NULL){
+				//no children
+				if(p){
+					if(p->left == c) p->left = NULL;
+					else p->right = NULL;
+				}
+				else root = NULL;
+				delete c;
+				return;
+			}
+			else if(c->left && c->right == NULL){
+				//only left child exists
+				if(p){
+					if(p->left == c) p->left = c->left;
+					else p->right = c->left;
+				}
+				else root = c->left;
+				delete c;
+			}
+			else if(c->right && c->left == NULL){
+				//only right child exists
+				if(p){
+					if(p->left == c) p->left = c->right;
+					else p->right = c->right;
+				}
+				else root = c->right;
+				delete c;
+			}
+			else{
+				//both children exist
+				//pick the largest value of the left subtree
+				Node* l = c->left, *lp = c;
+				while(l->right){
+					lp = l;
+					l = l->right;
+				}
+				c->data = l->data;
+				remove_node(l,lp,l->data);
+			}
+		}
 	}
 public:
 	BST(){
@@ -66,70 +108,61 @@ public:
 			prev->left = new Node(v);
 	}
 	Node* find(int v){
-		Pair p = find_parent(v,root);
-		if(!p.first) return NULL;
-		if(p.second->data <= v)
-			return p.second->right;
-		else
-			return p.second->left;
+		Node* p = NULL;
+		Node* c = root;
+		while(c != NULL){
+			if(c->data == v) return c;
+			else if(c->data < v){
+				p = c;
+				c = c->right;
+			}
+			else{
+				p = c;
+				c = c->left;
+			}
+		}
+		return NULL;
 	}
-	void remove(int v,Node* s){
-		if(s == NULL) s = root;
-		Pair p = find_parent(v,s);
-		if(!p.first) return;
-		Node* c;
-		int side;
-
-		if(p.second->data <= v){
-			side = 1;
-			c = p.second->right;
+	void remove(int v){
+		remove_node(root,NULL,v);
+	}
+	void level_order(){
+		queue<Node*> q;
+		q.push(root);
+		q.push(NULL);
+		while(!q.empty()){
+			Node* f = q.front();
+			q.pop();
+			if(f == NULL){
+				cout<<endl;
+				if(!q.empty())
+					q.push(NULL);
+			}
+			else{
+				cout<<f->data<<" ";
+				if(f->left) q.push(f->left);
+				if(f->right) q.push(f->right);
+			}
 		}
-		else{
-			side = 0;
-			c = p.second->left;
-		}
-		//only 1 child
-		if(c->left == NULL){
-			if(side)
-				p.second->right = c->right;
-			else
-				p.second->left = c->right;
-			delete c;
-			return;
-		}
-		if(c->right == NULL){
-			if(side)
-				p.second->right = c->left;
-			else
-				p.second->left = c->left;
-			delete c;
-			return;
-		}
-		//2 childeren
-		// pick the largest from the left subtree
-		Node *pl = NULL, *l = c;
-		while(l->right != NULL){
-			pl = l;
-			l = l->right;
-		}
-		c->data = l->data;
-		remove(l->data,pl);
 	}
 };
 int main(){
 	BST b;
 	b.insert(4);
-	b.insert(3);
-	b.insert(5);
 	b.insert(2);
+	b.insert(6);
+	b.insert(1);
+    b.insert(3);
+    b.insert(5);
+    b.insert(7);
+    /*       4
+           /   \
+          2     6
+         / \   / \
+        1   3 5   7
+    */
 
-	Node* f = b.find(4);
-	if(f == NULL) cout<<"Not found!\n";
-	else cout<<"Found!\n";
-
-	f = b.find(5);
-	if(f == NULL) cout<<"Not found!\n";
-	else cout<<"Found!\n";
-
-	return 0;
+	b.level_order();
+	b.remove(2);
+	b.level_order();
 }
